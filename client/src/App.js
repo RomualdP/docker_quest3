47a04f01c89e6e12f3bb546a6115d12b517e3eb4
RomualdP <romuald.piquet@gmail.com>
2022-10-20 10:55:59 +0200

    fixing hot reload

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test du hot reload
+          Test du hot reload bis
         </a>
       </header>
     </div>
